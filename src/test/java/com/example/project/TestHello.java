68f74394eda394caa44285fa59b92102e888294e
yena5511 <ar01077422264@gmail.com>
2025-03-11 10:07:13 +0900

    테스트 성공

--- src/test/java/com/example/project/TestHello.java
+++ src/test/java/com/example/project/TestHello.java
@@ -2,29 +2,28 @@ package com.example.project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class TestHello {
 
-   @Test
-   public void testHelloWorld()
-   {
-     PrintStream originalOut = System.out;
-     ByteArrayOutputStream bos = new ByteArrayOutputStream();
-     System.setOut(new PrintStream(bos));
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 
-     // action
-     Hello.main(null);
+    @Test
+    public void testHelloWorld() {
 
-     // assertion
-     assertEquals("Hello world!\n", bos.toString());
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
 
-     // undo the binding in System
-     System.setOut(originalOut);
-   }
+        main(null);
+
+        assertEquals("Hello world!\n", bos.toString());
+
+        System.setOut(originalOut);
+    }
 }
